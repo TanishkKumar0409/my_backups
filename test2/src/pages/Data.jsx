355febae5f195115d70e503bb310a511de12c66f
TanishkKumar0409 <tanishkk60@gmail.com>
2024-08-29 19:02:05 +0530

    do some designing

--- test2/src/pages/Data.jsx
+++ test2/src/pages/Data.jsx
@@ -19,10 +19,30 @@ export default function Data() {
   return (
     <>
       <section>
-        <h1>{data.map((item)=>{          return(
-            <p key={item.id}>{item.name}</p>
-          )
-})}</h1>
+        <h1>
+
+          {data.map((item) => {
+            return (
+
+              <p key={item.id}>
+                Hello, my name is <span>{item.name}</span>, and my username is{" "}
+                <span>{item.username}</span>. My ID is <span>{item.id}</span>. I
+                reside at <span>{item.address.street}</span>,{" "}
+                <span>{item.address.suite}</span>,{" "}
+                <span>{item.address.city}</span>, with the zipcode{" "}
+                <span>{item.address.zipcode}</span>. You can reach me via email
+                at <span>{item.email}</span> or call me at{" "}
+                <span>{item.phone}</span>. My personal website is{" "}
+                <span>{item.website}</span>. I work at{" "}
+                <span>{item.company.name}</span>, a company known for its "
+                <span>{item.company.catchPhrase}</span>" and our focus on "
+                <span>{item.company.bs}</span>".
+              </p>
+              
+            );
+          })}
+          
+        </h1>
       </section>
     </>
   );
