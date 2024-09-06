06ed6fac87d2ada5cfe7692b4c84315670a422ae
TanishkKumar0409 <tanishkk60@gmail.com>
2024-09-06 19:06:52 +0530

    update test4

--- test4/src/Pages/DataShow.jsx
+++ test4/src/Pages/DataShow.jsx
@@ -8,50 +8,46 @@ export default function DataShow(props) {
   useEffect(() => {
     getData();
   }, []);
+
   const getData = async () => {
     const out = await fetch("https://jsonplaceholder.typicode.com/users");
     const output = await out.json();
     setData(output);
   };
+
   return (
     <>
       {data.length > 0 ? (
-        <>
-          <section className="container MultiPageMain d-flex justify-content-center align-items-center">
-            <div className="w-100">
-              <table className="table table-striped table-bordered w-100">
-                <thead>
-                  <tr>
-                    <th>Id</th>
-                    <th>Name</th>
-                    <th>UserName</th>
-                    <th>Email</th>
-                    <th>View</th>
+        <section className="container MultiPageMain d-flex justify-content-center align-items-center">
+          <div className="w-100">
+            <table className="table table-striped table-bordered w-100">
+              <thead>
+                <tr>
+                  <th>Id</th>
+                  <th>Name</th>
+                  <th>UserName</th>
+                  <th>Email</th>
+                  <th>View</th>
+                </tr>
+              </thead>
+              <tbody>
+                {data.map((item) => (
+                  <tr key={item.id}>
+                    <td>{item.id}</td>
+                    <td>{item.name}</td>
+                    <td>{item.username}</td>
+                    <td>{item.email}</td>
+                    <td>
+                      <Link to={`/data/view/${item.id}`}>
+                        <button className="btn btn-primary">View</button>
+                      </Link>
+                    </td>
                   </tr>
-                </thead>
-                <tbody>
-                  {data.map((item) => {
-                    return (
-                      <>
-                        <tr key={item.id}>
-                          <td>{item.id}</td>
-                          <td>{item.name}</td>
-                          <td>{item.username}</td>
-                          <td>{item.email}</td>
-                          <td>
-                            <Link to={`/data/view/${item.id}`}>
-                              <button className="btn btn-primary">View</button>
-                            </Link>
-                          </td>
-                        </tr>
-                      </>
-                    );
-                  })}
-                </tbody>
-              </table>
-            </div>
-          </section>
-        </>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </section>
       ) : (
         <Loader />
       )}
