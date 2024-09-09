fb7a8530f22391fcb2156980558d68f75821783c
TanishkKumar0409 <tanishkk60@gmail.com>
2024-09-09 18:55:48 +0530

    update test4

--- test4/src/Components/Loader.jsx
+++ test4/src/Components/Loader.jsx
@@ -3,7 +3,10 @@ import React from "react";
 export default function Loader(props) {
   return (
     <>
-      <section className="MultiPageMain d-flex justify-content-center align-items-center" style={props.RootColors}>
+      <section
+        className="MultiPageMain d-flex justify-content-center align-items-center"
+        style={props.RootColors}
+      >
         <div class="spinner-grow text-light" role="status">
           <span class="visually-hidden">Loading...</span>
         </div>
