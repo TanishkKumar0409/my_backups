767f4b38bb62f97b8346c2bfbd4ae3e5c6d64b9d
TanishkKumar0409 <tanishkk60@gmail.com>
2024-09-14 18:21:42 +0530

    update test3

--- test3/src/App.js
+++ test3/src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import "./App.css";
 import Navbar from "./Component/Navbar";
 import Hero from "./Component/Hero";
+import Projects from "./Component/Project";
 
 function App() {
   return <>
   <Navbar />
   <Hero />
+  <Projects />
   </>;
 }
 
---
+++ test3/src/Component/Project.jsx
@@ -0,0 +1,329 @@
+import React from "react";
+
+export default function Projects() {
+  return (
+    <>
+      <section id="projects" class="py-5 bg-dark text-light">
+        <div class="container">
+          <h2 class="text-center mb-4">Projects</h2>
+          <div class="row g-4">
+            {/* <!-- Project 1: Orgaawe with modal trigger --> */}
+            <div class="col-md-3">
+              <div class="card h-100">
+                <img
+                  src="https://orgaawe.com/image/logo.png"
+                  class="card-img-top img-fluid"
+                  alt="Orgaawe Project"
+                  style={{ height: "200px", objectFit: "cover" }}
+                />
+                <div class="card-body">
+                  <h5 class="card-title">Orgaawe</h5>
+                  <p class="card-text">
+                    A project focusing on organic food and eco-friendly
+                    products.
+                  </p>
+                  <button
+                    class="btn btn-primary"
+                    data-bs-toggle="modal"
+                    data-bs-target="#orgaaweModal"
+                  >
+                    <i class="fas fa-eye"></i> View Project
+                  </button>
+                </div>
+              </div>
+            </div>
+
+            {/* <!-- Project 1 Modal --> */}
+            <div
+              class="modal fade"
+              id="orgaaweModal"
+              tabindex="-1"
+              aria-labelledby="orgaaweModalLabel"
+              aria-hidden="true"
+            >
+              <div class="modal-dialog modal-lg modal-dialog-centered">
+                <div class="modal-content bg-dark text-light">
+                  <div class="modal-header">
+                    <h5 class="modal-title" id="orgaaweModalLabel">
+                      Orgaawe Project Details
+                    </h5>
+                    <button
+                      type="button"
+                      class="btn-close btn-close-light"
+                      data-bs-dismiss="modal"
+                      aria-label="Close"
+                    ></button>
+                  </div>
+                  <div class="modal-body text-center">
+                    <img
+                      src="https://orgaawe.com/image/logo.png"
+                      class="img-fluid"
+                      alt="Orgaawe Project Image"
+                      style={{maxHeight: "300px", objectFit: "cover"}}
+                    />
+                    <p class="mt-3">
+                      This project focuses on organic and sustainable products.
+                    </p>
+                    <p>Technologies used: HTML, CSS, JavaScript, PHP.</p>
+                  </div>
+                  <div class="modal-footer">
+                    <button
+                      type="button"
+                      class="btn btn-secondary"
+                      data-bs-dismiss="modal"
+                    >
+                      <i class="fas fa-times"></i> Close
+                    </button>
+                    <a
+                      href="https://orgaawe.com"
+                      target="_blank"
+                      class="btn btn-primary"
+                    >
+                      <i class="fas fa-link"></i> View Live Project
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            {/* <!-- Project 2: Macs Concepts with modal trigger --> */}
+            <div class="col-md-3">
+              <div class="card h-100">
+                <img
+                  src="https://macsconcepts.in/assets/images/logo/macs-logo.png"
+                  class="card-img-top img-fluid"
+                  alt="Macs Concepts Project"
+                  style={{height: "200px", objectFit: "cover"}}
+                />
+                <div class="card-body">
+                  <h5 class="card-title">Macs Concepts</h5>
+                  <p class="card-text">
+                    A corporate branding and design consultancy website.
+                  </p>
+                  <button
+                    class="btn btn-primary"
+                    data-bs-toggle="modal"
+                    data-bs-target="#macsconceptsModal"
+                  >
+                    <i class="fas fa-eye"></i> View Project
+                  </button>
+                </div>
+              </div>
+            </div>
+
+            {/* <!-- Project 2 Modal --> */}
+            <div
+              class="modal fade"
+              id="macsconceptsModal"
+              tabindex="-1"
+              aria-labelledby="macsconceptsModalLabel"
+              aria-hidden="true"
+            >
+              <div class="modal-dialog modal-lg modal-dialog-centered">
+                <div class="modal-content bg-dark text-light">
+                  <div class="modal-header">
+                    <h5 class="modal-title" id="macsconceptsModalLabel">
+                      Macs Concepts Project Details
+                    </h5>
+                    <button
+                      type="button"
+                      class="btn-close btn-close-light"
+                      data-bs-dismiss="modal"
+                      aria-label="Close"
+                    ></button>
+                  </div>
+                  <div class="modal-body text-center">
+                    <img
+                      src="https://macsconcepts.in/assets/images/logo/macs-logo.png"
+                      class="img-fluid"
+                      alt="Macs Concepts Project Image"
+                      style={{maxHeight: "300px", objectFit: "cover"}}
+                    />
+                    <p class="mt-3">
+                      A corporate branding project with a focus on design.
+                    </p>
+                    <p>Technologies used: HTML, CSS, JavaScript.</p>
+                  </div>
+                  <div class="modal-footer">
+                    <button
+                      type="button"
+                      class="btn btn-secondary"
+                      data-bs-dismiss="modal"
+                    >
+                      <i class="fas fa-times"></i> Close
+                    </button>
+                    <a
+                      href="https://macsconcepts.in"
+                      target="_blank"
+                      class="btn btn-primary"
+                    >
+                      <i class="fas fa-link"></i> View Live Project
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            {/* <!-- Project 3: Back to Basics with modal trigger --> */}
+            <div class="col-md-3">
+              <div class="card h-100">
+                <img
+                  src="https://thebacktobasics.com/images/logo/B2Blogo.png"
+                  class="card-img-top img-fluid"
+                  alt="Back to Basics Project"
+                   style={{height: "200px", objectFit: "cover"}}
+                />
+                <div class="card-body">
+                  <h5 class="card-title">Back to Basics</h5>
+                  <p class="card-text">
+                    A lifestyle brand focused on simplicity and minimalism.
+                  </p>
+                  <button
+                    class="btn btn-primary"
+                    data-bs-toggle="modal"
+                    data-bs-target="#backtobasicsModal"
+                  >
+                    <i class="fas fa-eye"></i> View Project
+                  </button>
+                </div>
+              </div>
+            </div>
+
+            {/* <!-- Project 3 Modal --> */}
+            <div
+              class="modal fade"
+              id="backtobasicsModal"
+              tabindex="-1"
+              aria-labelledby="backtobasicsModalLabel"
+              aria-hidden="true"
+            >
+              <div class="modal-dialog modal-lg modal-dialog-centered">
+                <div class="modal-content bg-dark text-light">
+                  <div class="modal-header">
+                    <h5 class="modal-title" id="backtobasicsModalLabel">
+                      Back to Basics Project Details
+                    </h5>
+                    <button
+                      type="button"
+                      class="btn-close btn-close-light"
+                      data-bs-dismiss="modal"
+                      aria-label="Close"
+                    ></button>
+                  </div>
+                  <div class="modal-body text-center">
+                    <img
+                      src="https://thebacktobasics.com/images/logo/B2Blogo.png"
+                      class="img-fluid"
+                      alt="Back to Basics Project Image"
+                      style={{maxHeight: "300px", objectFit: "cover"}}
+                    />
+                    <p class="mt-3">
+                      This project is centered on simple, minimalistic lifestyle
+                      products.
+                    </p>
+                    <p>Technologies used: HTML, CSS, JavaScript.</p>
+                  </div>
+                  <div class="modal-footer">
+                    <button
+                      type="button"
+                      class="btn btn-secondary"
+                      data-bs-dismiss="modal"
+                    >
+                      <i class="fas fa-times"></i> Close
+                    </button>
+                    <a
+                      href="https://thebacktobasics.com"
+                      target="_blank"
+                      class="btn btn-primary"
+                    >
+                      <i class="fas fa-link"></i> View Live Project
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            {/* <!-- Project 4: Offbeat Pixels with modal trigger --> */}
+            <div class="col-md-3">
+              <div class="card h-100">
+                <img
+                  src="https://offbeatpixels.com/images/offbeat-pixel.png"
+                  class="card-img-top img-fluid"
+                  alt="Offbeat Pixels Project"
+                   style={{height: "200px", objectFit: "cover"}}
+                />
+                <div class="card-body">
+                  <h5 class="card-title">Offbeat Pixels</h5>
+                  <p class="card-text">
+                    A creative design studio specializing in web development.
+                  </p>
+                  <button
+                    class="btn btn-primary"
+                    data-bs-toggle="modal"
+                    data-bs-target="#offbeatpixelsModal"
+                  >
+                    <i class="fas fa-eye"></i> View Project
+                  </button>
+                </div>
+              </div>
+            </div>
+
+            {/* <!-- Project 4 Modal --> */}
+            <div
+              class="modal fade"
+              id="offbeatpixelsModal"
+              tabindex="-1"
+              aria-labelledby="offbeatpixelsModalLabel"
+              aria-hidden="true"
+            >
+              <div class="modal-dialog modal-lg modal-dialog-centered">
+                <div class="modal-content bg-dark text-light">
+                  <div class="modal-header">
+                    <h5 class="modal-title" id="offbeatpixelsModalLabel">
+                      Offbeat Pixels Project Details
+                    </h5>
+                    <button
+                      type="button"
+                      class="btn-close btn-close-light"
+                      data-bs-dismiss="modal"
+                      aria-label="Close"
+                    ></button>
+                  </div>
+                  <div class="modal-body text-center">
+                    <img
+                      src="https://offbeatpixels.com/images/offbeat-pixel.png"
+                      class="img-fluid"
+                      alt="Offbeat Pixels Project Image"
+                      style={{maxHeight: "300px", objectFit: "cover"}}
+                    />
+                    <p class="mt-3">
+                      Creative web design and development with a focus on
+                      innovation.
+                    </p>
+                    <p>Technologies used: HTML, CSS, JavaScript, PHP.</p>
+                  </div>
+                  <div class="modal-footer">
+                    <button
+                      type="button"
+                      class="btn btn-secondary"
+                      data-bs-dismiss="modal"
+                    >
+                      <i class="fas fa-times"></i> Close
+                    </button>
+                    <a
+                      href="https://offbeatpixels.com"
+                      target="_blank"
+                      class="btn btn-primary"
+                    >
+                      <i class="fas fa-link"></i> View Live Project
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
