793cdfe1df418a296559accab49e69f7f9a2d8e8
TanishkKumar0409 <tanishkk60@gmail.com>
2024-11-28 09:35:18 +0530

    updat

--- Node/CreateingServer/index.js
+++ Node/CreateingServer/index.js
@@ -1,7 +1,9 @@
 import http from "http";
-import fs from "fs"
+import fs from "fs";
 
 const app = http.createServer((req, res) => {
+  const log = `You got a request from ${req.url} at ${Date.now()}`;
+  fs.appendFile("log.txt", log, () => {});
   res.end("Hello i am Server");
 });
 
