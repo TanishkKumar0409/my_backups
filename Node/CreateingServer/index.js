84d4b95761a8ec639a076e3394414e4aca3db863
TanishkKumar0409 <tanishkk60@gmail.com>
2024-11-28 09:36:15 +0530

    updat

--- Node/CreateingServer/index.js
+++ Node/CreateingServer/index.js
@@ -3,8 +3,13 @@ import fs from "fs";
 
 const app = http.createServer((req, res) => {
   const log = `You got a request from ${req.url} at ${Date.now()}`;
-  fs.appendFile("log.txt", log, () => {});
-  res.end("Hello i am Server");
+  fs.appendFile("log.txt", log, (error) => {
+    if (!error) {
+      res.end("Hello i am Server");
+    } else if (error) {
+      res.end(error);
+    }
+  });
 });
 
 app.listen(5000, () => console.log(`Server Running at http://localhost:5000`));
