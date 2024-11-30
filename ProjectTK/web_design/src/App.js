e7fe83f64545cde8714488251210e8254be2bcc7
TanishkKumar0409 <tanishkk60@gmail.com>
2024-11-30 18:41:20 +0530

    update

--- ProjectTK/web_design/src/App.js
+++ ProjectTK/web_design/src/App.js
@@ -2,10 +2,29 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.js";
 import "./App.css";
 
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "./Pages/Home/Home";
+import Login from "./Pages/Forms/Login/Login";
+import Register from "./Pages/Forms/Register/Register";
+import Main from "./Pages/Main/Main";
+import UserHistory from "./Pages/UserHistory/UserHistory";
+import UserStorage from "./Pages/UserStorage/UserStorage";
+
 function App() {
   return (
     <>
-      <h1>Hello</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+
+          <Route path="/main" element={<Main />} />
+          <Route path="/main/history" element={<UserHistory />} />
+          <Route path="main/storage" element={<UserStorage />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
