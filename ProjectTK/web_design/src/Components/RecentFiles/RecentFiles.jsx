3ef235c9b40dfa96cc167555aebe70df6aaf6c80
TanishkKumar0409 <tanishkk60@gmail.com>
2024-12-08 18:48:30 +0530

    update

--- ProjectTK/web_design/src/Components/RecentFiles/RecentFiles.jsx
+++ ProjectTK/web_design/src/Components/RecentFiles/RecentFiles.jsx
@@ -20,12 +20,12 @@ export default function RecentFiles() {
   };
 
   const urls = [
-    "file1.pdf", "file2.mp4", "file3.jpg", "file4.png", "file5.mp3", 
-    "file6.html", "file7.css", "file8.js", "file9.java", "file10.txt", 
-    "file11.docx", "file12.xlsx", "file13.pptx", "file14.csv", "file15.xml", 
+    "file1.pdf", "file2.mp4", "file3.jpg", "file4.png", "file5.mp3",
+    "file6.html", "file7.css", "file8.js", "file9.java", "file10.txt",
+    "file11.docx", "file12.xlsx", "file13.pptx", "file14.csv", "file15.xml",
     "file16.json", "file17.php", "file18.ts", "file19.py", "file20.gif"
   ];
-  
+
 
   const getFileIcon = (fileName) => {
     const extension = fileName.split('.').pop().toLowerCase();
@@ -55,28 +55,30 @@ export default function RecentFiles() {
 
 
   return (
-    <section className="container mt-5">
-      <div className="row">
-        <h2 className="text-center mb-4 mainHeading text-uppercase fw-bold" style={{ "--text": "'Recent Files'" }}>Recent Files</h2>
-        <p className="px-5 text-center">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ea eveniet tempora, eius cumque necessitatibus nihil.</p>
-        <div className="col">
-          <OwlCarousel className="owl-theme" {...owlOptions}>
-            {urls.map((item, index) => (
-              <div className="item" key={index}>
-                <div className="cardCustom rounded-3 overflow-hidden bg-white">
-                  <div className="cardCustomHead h-50 d-flex justify-content-center align-items-center">
-                    <i className={`fa text-light fa-beat-fade ${getFileIcon(item)}`}></i>
-                  </div>
-                  <div className="cardCustomBody h-50 d-flex justify-content-center align-items-center text-center p-4 flex-column">
-                    <h2 className="fs-5 fw-bold text-break text-capitalize">{item}</h2>
-                    <button className="btn custom-btn btn-custom border-0 mt-3 overflow-hidden">View</button>
+    <>
+      <section className="container mt-5 bg-white">
+        <div className="row">
+          <h2 className="text-center mb-4 mainHeading text-uppercase fw-bold" style={{ "--text": "'Recent Files'" }}>Recent Files</h2>
+          <p className="px-5 text-center">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ea eveniet tempora, eius cumque necessitatibus nihil.</p>
+          <div className="col">
+            <OwlCarousel className="owl-theme" {...owlOptions}>
+              {urls.map((item, index) => (
+                <div className="item" key={index}>
+                  <div className="cardCustom rounded-3 overflow-hidden bg-white">
+                    <div className="cardCustomHead h-50 d-flex justify-content-center align-items-center">
+                      <i className={`fa text-light fa-beat-fade ${getFileIcon(item)}`}></i>
+                    </div>
+                    <div className="cardCustomBody h-50 d-flex justify-content-center align-items-center text-center p-4 flex-column">
+                      <h2 className="fs-5 fw-bold text-break text-capitalize">{item}</h2>
+                      <button className="btn custom-btn btn-custom border-0 mt-3 overflow-hidden">View</button>
+                    </div>
                   </div>
                 </div>
-              </div>
-            ))}
-          </OwlCarousel>
+              ))}
+            </OwlCarousel>
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </>
   );
 }
