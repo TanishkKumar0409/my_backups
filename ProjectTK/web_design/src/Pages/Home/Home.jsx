eb93104f6eed9dac53b2411d63fa378bf8842724
TanishkKumar0409 <tanishkk60@gmail.com>
2024-12-10 17:39:04 +0530

    update

--- ProjectTK/web_design/src/Pages/Home/Home.jsx
+++ ProjectTK/web_design/src/Pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Banner from "../../Components/Banner/Banner";
 import RecentFiles from "../../Components/RecentFiles/RecentFiles";
+import FileHistory from "../../Components/FileHistory/FileHistory";
 
 export default function Home() {
   return (
     <>
       <Banner />
       <RecentFiles />
+      <FileHistory />
     </>
   );
 }
