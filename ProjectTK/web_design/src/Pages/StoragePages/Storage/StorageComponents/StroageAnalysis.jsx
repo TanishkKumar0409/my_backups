c070eeb0cb780e73d2167a0c7f830bebc9b8d422
TanishkKumar0409 <tanishkk60@gmail.com>
2024-12-24 19:18:15 +0530

    update

--- ProjectTK/web_design/src/Pages/StoragePages/Storage/StorageComponents/StroageAnalysis.jsx
+++ ProjectTK/web_design/src/Pages/StoragePages/Storage/StorageComponents/StroageAnalysis.jsx
@@ -26,16 +26,20 @@ export default function StorageAnalysis() {
         return () => clearInterval(intervalId);
     }, [username]);
 
-
     const usedSize = data.usedStorage || 0;
     const totalSize = data.totalStorage || 1;
 
-    const percentage = parseFloat(((usedSize / totalSize) * 100).toFixed(2));
+    let percentage = parseFloat(((usedSize / totalSize) * 100).toFixed(2));
+    if (percentage === 0) {
+        percentage = 101;
+    }
 
     const value = 628 - (628 * (percentage / 100));
 
     useEffect(() => {
-        if (percentage <= 20) {
+        if (percentage > 100) {
+            setGradientColors({ start: "#d3d3d3", end: "#a9a9a9" });
+        } else if (percentage <= 20) {
             setGradientColors({ start: "#00bcd4", end: "#2196f3" });
         } else if (percentage <= 40) {
             setGradientColors({ start: "#4caf50", end: "#8bc34a" });
@@ -43,57 +47,50 @@ export default function StorageAnalysis() {
             setGradientColors({ start: "#ffc107", end: "#ff9800" });
         } else if (percentage <= 80) {
             setGradientColors({ start: "#ff5722", end: "#f44336" });
-        } else {
-            setGradientColors({ start: "#9c27b0", end: "#673ab7" });
         }
     }, [percentage]);
 
     return (
-        <>
-            <section className='bg-white py-5'>
-                <div className="container">
-                    <h2 className="text-center mb-4 mainHeading text-uppercase fw-bold" style={{ "--text": `'${username} Storage'` }}>
-                        {username} Storage
-                    </h2>
-                    <div className="row py-3">
-
-
-                        <div className="col-md-6 textJustify">
-                            <p>
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni nisi deserunt exercitationem quos. Officiis quaerat minus earum quisquam deserunt necessitatibus nesciunt facere dolores. Non architecto ad suscipit, rerum nisi nesciunt harum maxime animi dolorem quos fuga deserunt impedit! Hic architecto incidunt fugiat nam laborum repudiandae perspiciatis nulla dicta beatae numquam animi cupiditate nesciunt, necessitatibus quo ipsum. Necessitatibus neque ullam voluptatem dicta quisquam, vitae assumenda quo. Eius aperiam nihil, aspernatur quidem nam labore veritatis, ad cumque non ipsam ducimus sit! Cum et veniam libero ipsum nisi rem. Necessitatibus, nesciunt, similique consequuntur commodi mollitia nihil fugit molestiae et obcaecati fugiat ducimus facere.
-                            </p>
-                        </div>
-
-                        <div className="col-md-6 d-flex justify-content-center">
-                            <div className="skill position-relative">
-                                <div className="outer rounded-circle">
-                                    <div className="inner rounded-circle d-flex justify-content-center align-items-center">
-                                        <div id="number" className='fw-bold'>
-                                            {percentage}%
-                                        </div>
+        <section className='bg-white py-5'>
+            <div className="container">
+                <h2 className="text-center mb-4 mainHeading text-uppercase fw-bold" style={{ "--text": `'${username} Storage'` }}>
+                    {username} Storage
+                </h2>
+                <div className="row py-3">
+                    <div className="col-md-6 textJustify">
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni nisi deserunt exercitationem quos. Officiis quaerat minus earum quisquam deserunt necessitatibus nesciunt facere dolores. Non architecto ad suscipit, rerum nisi nesciunt harum maxime animi dolorem quos fuga deserunt impedit! Hic architecto incidunt fugiat nam laborum repudiandae perspiciatis nulla dicta beatae numquam animi cupiditate nesciunt, necessitatibus quo ipsum. Necessitatibus neque ullam voluptatem dicta quisquam, vitae assumenda quo. Eius aperiam nihil, aspernatur quidem nam labore veritatis, ad cumque non ipsam ducimus sit! Cum et veniam libero ipsum nisi rem. Necessitatibus, nesciunt, similique consequuntur commodi mollitia nihil fugit molestiae et obcaecati fugiat ducimus facere.
+                        </p>
+                    </div>
+                    <div className="col-md-6 d-flex justify-content-center">
+                        <div className="skill position-relative">
+                            <div className="outer rounded-circle">
+                                <div className="inner rounded-circle d-flex justify-content-center align-items-center">
+                                    <div id="number" className='fw-bold'>
+                                        {percentage > 100 ? "0" : percentage}%
                                     </div>
                                 </div>
-                                <svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="200px" height="200px" className='position-absolute top-0 start-0'>
-                                    <defs>
-                                        <linearGradient id="GradientColor">
-                                            <stop offset="0%" stopColor={gradientColors.start} />
-                                            <stop offset="100%" stopColor={gradientColors.end} />
-                                        </linearGradient>
-                                    </defs>
-                                    <circle
-                                        id="circle"
-                                        cx="100"
-                                        cy="100"
-                                        r="90"
-                                        strokeLinecap="round"
-                                        style={{ "--array": value }}
-                                    />
-                                </svg>
                             </div>
+                            <svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="200px" height="200px" className='position-absolute top-0 start-0'>
+                                <defs>
+                                    <linearGradient id="GradientColor">
+                                        <stop offset="0%" stopColor={gradientColors.start} />
+                                        <stop offset="100%" stopColor={gradientColors.end} />
+                                    </linearGradient>
+                                </defs>
+                                <circle
+                                    id="circle"
+                                    cx="100"
+                                    cy="100"
+                                    r="90"
+                                    strokeLinecap="round"
+                                    style={{ "--array": value }}
+                                />
+                            </svg>
                         </div>
                     </div>
                 </div>
-            </section>
-        </>
+            </div>
+        </section>
     );
 }
