05dc093f061193a401e8178325532a04c051e6e4
TanishkKumar0409 <tanishkk60@gmail.com>
2024-12-27 18:08:55 +0530

    update

--- ProjectTK/web_design/src/Pages/Home/HomeComponents/Features/Features.jsx
+++ ProjectTK/web_design/src/Pages/Home/HomeComponents/Features/Features.jsx
@@ -4,6 +4,8 @@ import "owl.carousel/dist/assets/owl.carousel.css";
 import "owl.carousel/dist/assets/owl.theme.default.css";
 import img from "../../../../Image/Wave.jpg";
 
+import MyFeatures from "./MyFeatures.json"
+
 export default function Features() {
     const owlOptions = {
         loop: true,
@@ -19,48 +21,30 @@ export default function Features() {
         },
     };
 
-    // Array of features as objects
-    const features = [
-        {
-            title: "Secure File Upload",
-            description: "Easily upload your files with end-to-end encryption to ensure data safety.",
-            iconClass: "fa fa-cloud-upload",
-            cardBg: "#06b6d4"
-        },
-        {
-            title: "Large Storage Capacity",
-            description: "Store large files and access them from anywhere, anytime.",
-            iconClass: "fa fa-database",
-            cardBg: "#ef4444"
-        },
-        {
-            title: "File Sharing",
-            description: "Share files securely with specific users or through public links.",
-            iconClass: "fa fa-share-alt",
-            cardBg: "#fbbf24"
-        },
-        {
-            title: "Version Control",
-            description: "Track file changes and access previous versions as needed.",
-            iconClass: "fa fa-history",
-            cardBg: "#10b981"
-        },
-        {
-            title: "Real-Time Collaboration",
-            description: "Work together on documents with teammates in real-time.",
-            iconClass: "fa fa-users",
-            cardBg: "#a78bfa"
-        },
-    ];
+    const getColor = (index) => {
+        switch ((index + 1) % 5) {
+            case 0:
+                return "#a78bfa";
+            case 4:
+                return "#10b981"; 
+            case 3:
+                return "#fbbf24"; 
+            case 2:
+                return "#ef4444"; 
+            default:
+                return "#06b6d4";
+        }
+    }
+
 
     return (
         <section className="bg-white py-5">
             <div className="container">
                 <h2 className="text-center mb-4 mainHeading text-uppercase fw-bold" style={{ "--text": "'Features'" }}>Features</h2>
                 <OwlCarousel className="owl-theme" {...owlOptions}>
-                    {features.map((feature, index) => (
+                    {MyFeatures.map((feature, index) => (
                         <div className="item" key={index}>
-                            <div className="featureCard position-relative rounded overflow-hidden border" style={{ "--cardColor": feature.cardBg }}>
+                            <div className="featureCard position-relative rounded overflow-hidden border" style={{ "--cardColor": getColor(index) }}>
                                 <img
                                     src={img}
                                     className="position-absolute top-0 start-0 w-100 h-100 featureImg"
